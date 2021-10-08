4d56acc7680dc73854287f9f8cc1710495fe2664
yohei yuri <yoppei0818@gmail.com>
2021-10-08 19:36:14 +0900

    Add 1item to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Memo.create(title: "サンプル用", description: "サンプル用のメモです。")
